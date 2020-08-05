8400c7e520cbcf338b32bfa0d1d606c3d014b14b
Ajmal <ajmalesa@gmail.com>
2020-08-05 01:23:40 -0600

    Revert "Attempting to use compiled JS file"
    This reverts commit 638ada64852e654be33f8d95a6285b115c5066ab.

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -49,7 +49,6 @@
             @yield('content')
         </div>
 
-        <script src="../seddit/public/js/voting.js"></script>
         <script src="{{ asset('js/voting.js') }}"></script>
         <script src="{{ asset('js/comment.js') }}"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
