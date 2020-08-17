a70ef98c1cbb19ee852e1cc9d78de483b7998dc7
AJEsa <ajmal@marketapts.com>
2020-08-17 12:11:31 -0600

    Comment model can now grab username of user who posted comment

--- app/Comment.php
+++ app/Comment.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    //
+    public function getUserName() {
+        $user = User::where('id', $this->user_id)->get('username');
+
+        return $user[0]->original['username'];
+    }
 }
