baa097b3279687c391b506bce0714ca13b10e113
Ajmal <7179908+ajmalesa@users.noreply.github.com>
2020-08-27 05:08:51 -0600

    Reverted migrations file as they should not be changed

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('username')->unique();
+            $table->string('username');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
