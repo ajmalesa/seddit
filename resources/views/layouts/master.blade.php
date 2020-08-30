7c6e955b07007386ce9e8b9e62a79ca3c24a5b28
Ajmal <7179908+ajmalesa@users.noreply.github.com>
2020-08-30 16:01:25 -0600

    Added meta description

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,6 +4,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <meta name="description" content="Reddit clone made to learn Laravel and MVC. The title Seddit is a pun on the name Reddit, which is a pun itself.">
         <meta name="csrf-token" content="{{ csrf_token() }}" />
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <link href="{{ asset('css/style.css?v='.filemtime('css/style.css')) }}" rel="stylesheet"/>
