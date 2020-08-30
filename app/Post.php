7a2b018ef7ae3d2262446facbebce45c5c342240
Ajmal <7179908+ajmalesa@users.noreply.github.com>
2020-08-30 13:05:31 -0600

    Post model can now grab the userID by the given author name

--- app/Post.php
+++ app/Post.php
@@ -3,6 +3,8 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use DB;
 
 class Post extends Model
 {
@@ -10,4 +12,19 @@ class Post extends Model
         return $this->belongsToMany(User::class);
     }
 
+    /**
+     * Grab the userID for a post by the author name
+     *
+     * @param author the name of the author for the given post
+     */
+    public function getUserIDByAuthorName($author) {
+        $userID = DB::table('users')
+            ->where('username', $author)
+            ->get('id');
+
+        $userID = str_replace('[{"id":', '', $userID);
+        $userID = str_replace('}]', '', $userID);
+
+        return $userID;
+    }
 }
